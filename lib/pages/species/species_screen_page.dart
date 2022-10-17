5e259300bac2a9ef8f505f9a795bfd793df34102
dafinoer <dafinoer@outlook.com>
2022-10-17 14:16:29 +0700

    set initializ store mobx

--- lib/pages/species/species_screen_page.dart
+++ lib/pages/species/species_screen_page.dart
@@ -1,5 +1,9 @@
-import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:lightsaber/component/injector.dart';
+import 'package:lightsaber/presentation/species/species_store.dart';
+import 'package:mobx/mobx.dart';
+import 'package:random_avatar/random_avatar.dart';
 
 class SpeciesScreenPage extends StatefulWidget {
   const SpeciesScreenPage({
@@ -11,10 +15,44 @@ class SpeciesScreenPage extends StatefulWidget {
 }
 
 class _SpeciesScreenPageState extends State<SpeciesScreenPage> {
+  late final SpeciesStore _speciesStore;
+
+  @override
+  void initState() {
+    super.initState();
+    _speciesStore = SpeciesStore(getIt.get());
+    _speciesStore.onFetchSpecies();
+    when((_) => _speciesStore.errorMessage != null, () {
+      if (_speciesStore.errorMessage != null) {
+        final snackBar = SnackBar(content: Text(_speciesStore.errorMessage!));
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: SizedBox.expand(child: Observer(
+        builder: (context) {
+          if (_speciesStore.isLoadingScreen) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: _speciesStore.species.length,
+            itemBuilder: (context, index) => ListTile(
+              leading: CircleAvatar(
+                child: randomAvatar('saytoonz', fit: BoxFit.cover),
+              ),
+              title: Text(_speciesStore.species[index].name),
+            ),
+          );
+        },
+      )),
     );
   }
 }
