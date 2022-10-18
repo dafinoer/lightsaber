f3609224e84fdb0cef2df72bcc0ae820ee85aa26
dafinoer <dafinoer@outlook.com>
2022-10-18 11:30:09 +0700

    design page

--- lib/pages/detail_screen_page/detail_screen_page.dart
+++ lib/pages/detail_screen_page/detail_screen_page.dart
@@ -1,22 +1,74 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:lightsaber/component/injector.dart';
+import 'package:lightsaber/pages/detail_screen_page/component/list_item.dart';
+import 'package:lightsaber/presentation/species_detail/species_detail_store.dart';
+import 'package:mobx/mobx.dart';
+import 'package:random_avatar/random_avatar.dart';
+import 'package:sizer/sizer.dart';
 
 class DetailScreenPage extends StatefulWidget {
-  const DetailScreenPage({Key? key, @PathParam('id') required this.idSpecies})
-      : super(key: key);
+  const DetailScreenPage({
+    Key? key,
+    @PathParam('id') required this.idSpecies,
+    @PathParam('idAvatar') required this.idAvatar,
+    @PathParam('name') required this.name,
+  }) : super(key: key);
 
   final int idSpecies;
+  final String idAvatar;
+  final String name;
 
   @override
   State<DetailScreenPage> createState() => _DetailScreenPageState();
 }
 
 class _DetailScreenPageState extends State<DetailScreenPage> {
+  late final SpeciesDetailStore _speciesDetailStore;
+
+  @override
+  void initState() {
+    super.initState();
+    _speciesDetailStore = SpeciesDetailStore(getIt.get());
+    _speciesDetailStore.fetchRepository('${widget.idSpecies}');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(child: Text('lorem')),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 35.h,
+            flexibleSpace: FlexibleSpaceBar(
+              background: randomAvatar(widget.idAvatar, trBackground: true),
+              title: Text(widget.name, style: theme.appBarTheme.titleTextStyle),
+            ),
+          ),
+          Observer(
+            builder: (context) {
+              switch (_speciesDetailStore.speciesFutureStatus) {
+                case FutureStatus.pending:
+                  return const SliverFillRemaining(
+                      child: Center(child: CircularProgressIndicator()));
+                case FutureStatus.fulfilled:
+                  if (_speciesDetailStore.speciesObject != null) {
+                    return ListItem(
+                        species: _speciesDetailStore.speciesObject!);
+                  }
+                  return const SliverFillRemaining(child: SizedBox.shrink());
+                case FutureStatus.rejected:
+                  return const SliverFillRemaining(
+                      child: Center(child: Text('Someting Wrong')));
+                default:
+                  return const SliverFillRemaining(child: SizedBox.shrink());
+              }
+            },
+          )
+        ],
+      ),
     );
   }
 }
