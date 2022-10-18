b012fdefd0c03b55c26e54e7a56b7951dcb5d180
dafinoer <dafinoer@outlook.com>
2022-10-18 12:36:04 +0700

    redesign splash screen

--- lib/pages/splash/splash_screen_page.dart
+++ lib/pages/splash/splash_screen_page.dart
@@ -1,4 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:lightsaber/component/routers.dart';
+import 'package:sizer/sizer.dart';
 
 class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({Key? key}) : super(key: key);
@@ -9,14 +12,71 @@ class SplashScreenPage extends StatefulWidget {
 
 class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 10000)).then((value) {
+      AutoRouter.of(context).replace(const SpeciesScreenRoute());
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return SizedBox.expand(
-      child: ColoredBox(
-          color: theme.backgroundColor,
-          child: const Center(
-            child: Text('splash'),
-          )),
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(180.0)),
+                    child: ColoredBox(
+                      color: const Color(0xFFFCB600),
+                      child: SizedBox.square(dimension: 20.h),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FlutterLogo(size: 7.h),
+                  const Padding(padding: EdgeInsets.symmetric(vertical: 4.0)),
+                  Text('May the 4th be with you',
+                      style: theme.textTheme.headline5
+                          ?.copyWith(fontWeight: FontWeight.bold)),
+                  const Padding(padding: EdgeInsets.symmetric(vertical: 4.0)),
+                  const CircularProgressIndicator(color: Color(0xFFFCB600),)
+                ],
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(360.0)),
+                      child: ColoredBox(
+                        color: const Color(0xFFFCB600),
+                        child: SizedBox.square(dimension: 30.h),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
